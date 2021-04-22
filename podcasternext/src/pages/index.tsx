41cf3826a0048b35dd501db6f93b36b6fb4f0786
Jacson Medeiros <jacsonmedeiros@gmail.com>
2021-04-22 07:37:00 -0300

    implementação do header e Player

--- podcasternext/src/pages/index.tsx
+++ podcasternext/src/pages/index.tsx
@@ -1,5 +1,44 @@
-export default function Home() {
+//SPA - SINGLE PAGE APLICATION -> dispara uma chamada quando algo ocorre na aplicação
+//SSR - SERVER SIDE RENDERING
+//SSG
+import { GetStaticProps } from 'next';
+import { api } from '../services/api';
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+}
+
+type HomeProps = {
+  episodes: Episode[];
+
+}
+
+export default function Home(props: HomeProps) {
+
+  console.log(props.episodes)
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    },
+  })
+
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
