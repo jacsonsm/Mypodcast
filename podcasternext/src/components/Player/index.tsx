bd6447fb3b15c1ce60813f7a26c18ad30a9a5576
Jacson Medeiros <jacsonmedeiros@gmail.com>
2021-04-30 00:14:30 -0300

    stylizações index style player

--- podcasternext/src/components/Player/index.tsx
+++ podcasternext/src/components/Player/index.tsx
@@ -13,7 +13,9 @@ export function Player() {
         episodeList,
         currentEpisodeIndex,
         isPlaying,
+        isLooping,
         togglePlay,
+        toggleLoop,
         setPlayingState,
         playNext,
         playPrevious,
@@ -86,6 +88,7 @@ export function Player() {
                         src={episode.url}
                         ref={audioRef}
                         autoPlay
+                        loop={isLooping}
                         onPlay={() => setPlayingState(true)}
                         onPause={() => setPlayingState(false)}
                     />
@@ -113,7 +116,12 @@ export function Player() {
                     <button type="button" onClick={playNext} disabled={!episode || !hasNext}>
                         <img src="/play-next.svg" alt="Tocar próxima" />
                     </button>
-                    <button type="button" disabled={!episode}>
+                    <button
+                        type="button"
+                        disabled={!episode}
+                        onClick={toggleLoop}
+                        className={isLooping ? styles.isActive : ''}
+                    >
                         <img src="/repeat.svg" alt="Repetir" />
                     </button>
                 </div>
